2b2d18604ba534c35de4e23d17c7e059749e87c8
Adam Hicks <thomas.adam.hicks@gmail.com>
2017-04-26 12:33:48 -0600

    setting up for PyPi publication

---
+++ setup.py
@@ -0,0 +1,13 @@
+from ditutils.core import setup
+setup(
+    name = 'morpheusapi',
+    packages = ['morpheusapi'],
+    version = '2.11.1',
+    description = 'A python wrapper for Morpheus APIs',
+    author = 'Adam Hicks',
+    author_email = 'thomas.adam.hicks@gmail.com',
+    url = 'https://github.com/tadamhicks/morpheus-python',
+    download_url = 'https://github.com/tadamhicks/morpheus-python/archive/2.11.1.tar.gz',
+    keywords = ['morpheus', 'api', 'morpheus data'],
+    classifiers = [],
+)
