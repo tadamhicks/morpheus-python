9ee56c6724a32a04a98ba90e1c54676900bc799b
Adam Hicks <thomas.adam.hicks@gmail.com>
2017-06-30 14:45:23 -0600

    working on billing parsing

--- morpheusapi/apps.py
+++ morpheusapi/apps.py
@@ -16,3 +16,24 @@ class App(Morpheus):
         Morpheus.__init__(self, baseurl, username, password)
         self.headers = {"Authorization": "BEARER " + self.access_token}
         self.endpoint = posixpath.join('api', 'provision-types')
+
+
+    def get_apps(self, id=None):
+    	
+    	if id:
+
+    		if not isinstance(id, six.string_types):
+    			id = str(id)
+
+    		apps_url = urljoin(
+    						self.baseurl,
+    						posixpath.join(self.endpoint, id)
+    					)
+
+    	else:
+
+    		apps_url = urljoin(self.baseurl, self.endpoint)
+
+    	response = requests.get(apps_url, headers=self.headers)
+
+    	return response.text
